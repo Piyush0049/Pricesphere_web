23ee28f8961432e1948ac3cb11e26343df24fd8c
piyush0049 <piyushjoshi81204@gmail.com>
2025-06-23 18:48:53 +0530

    code cleaning

--- src/actions/s3_actions.ts
+++ src/actions/s3_actions.ts
@@ -12,9 +12,9 @@ export const uploadImageToS3 = async (file: any, signedUrl: string) => {
             throw new Error("Failed to upload file to S3.");
         }
 
-        return response; // Adjust if the response has a specific data format you need
+        return response;
     } catch (error) {
         console.error("Error uploading to S3:", error);
-        throw error; // Re-throw error to be caught in handleCreatePost
+        throw error;
     }
 };
