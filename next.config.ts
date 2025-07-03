d5130d9ea1916de7a0260e74d1e0516c03fcb628
piyush0049 <piyushjoshi81204@gmail.com>
2025-07-03 23:33:56 +0530

    build errors resolve

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['unsplash.com', 'm.media-amazon.com', 'rukminim2.flixcart.com', 'assets.ajio.com', "assets.myntassets.com", "assets.sheinindia.in"],
+    domains: [
+      "unsplash.com",
+      "m.media-amazon.com",
+      "rukminim2.flixcart.com",
+      "assets.ajio.com",
+      "assets.myntassets.com",
+      "assets.sheinindia.in",
+    ],
+  },
+  webpack(config) {
+    config.resolve.alias["@" as string] = path.resolve(__dirname, "src");
+    return config;
   },
 };
 
